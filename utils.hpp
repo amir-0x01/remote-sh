401b870f39dfcf7ca32807cb04e27ba92218c6a6
Amir <amircpro@gmail.com>
2020-06-16 19:35:44 +0100

    Add files via upload

---
+++ utils.hpp
@@ -0,0 +1,27 @@
+#ifndef UTILS
+#define UTILS
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sys/time.h>
+#include <thread>
+#include <signal.h>
+#include <vector>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+#include <map>
+#include <cstring>
+
+//NETWORKING
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#endif
